6f2a2741cafad0875677d96740707fce026fdca3
Макаров Андрей Александрович <aamaka61@mts.ru>
2023-05-20 22:13:36 +0300

    fix comments

--- Lct2023.Mobile/Lct2023.Business/UserContext.cs
+++ Lct2023.Mobile/Lct2023.Business/UserContext.cs
@@ -2,6 +2,7 @@ namespace Lct2023.Business
 {
     public class UserContext
     {
-        public string AccessToken { get; set; }
+        public string AccessToken { get; set; } =
+            "29dbc1ae26aa097b8723f3d5c5b694fa8582f77e2ec149ab79423425ebad7cf83749aebb2da62002f12e2a57610bd2ba4a86649ddb9c0ce672ea50671df2cf19bac7465b453164f17d2dee2d2f5b60571d703ccd09e98b22443f705a801c04c9b9e6a18cb364923043fa47887a69287591bb11700051926dd05d791db4354eae";
     }
 }
