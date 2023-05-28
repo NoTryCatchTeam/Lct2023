8d322dd8609739368f15be73e572bb0725b418e4
Dmitry Ordenov <dordenov@artec3d.com>
2023-05-28 17:03:47 +0200

    added loader to signin and signup buttons

--- Lct2023.Mobile/Lct2023.Android/Fragments/Auth/AuthSignInFragment.cs
+++ Lct2023.Mobile/Lct2023.Android/Fragments/Auth/AuthSignInFragment.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Reactive.Disposables;
 using Android.OS;
 using Android.Views;
-using AndroidX.ConstraintLayout.Motion.Widget;
+using DynamicData.Binding;
 using Google.Android.Material.Button;
+using Google.Android.Material.ProgressIndicator;
 using Google.Android.Material.TextField;
 using Lct2023.Android.Helpers;
 using Lct2023.Android.Listeners;
+using Lct2023.Converters;
+using Lct2023.Definitions.Types;
 using Lct2023.ViewModels.Auth;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Binding;
@@ -21,12 +26,13 @@ public class AuthSignInFragment : BaseFragment
 
         var viewModel = (AuthViewModel)DataContext;
 
-        var parentMotionLayout = Activity.FindViewById<MotionLayout>(Resource.Id.auth_layout);
         var signIn = new SignIn(
             view.FindViewById<TextInputEditText>(Resource.Id.auth_signin_email_value),
             view.FindViewById<TextInputEditText>(Resource.Id.auth_signin_password_value),
             view.FindViewById<MaterialButton>(Resource.Id.auth_signin_sing_in),
-            view.FindViewById<MaterialButton>(Resource.Id.auth_signin_vk));
+            view.FindViewById<CircularProgressIndicator>(Resource.Id.auth_signin_sing_in_loader),
+            view.FindViewById<MaterialButton>(Resource.Id.auth_signin_vk),
+            view.FindViewById<CircularProgressIndicator>(Resource.Id.auth_signin_singin_vk_loader));
 
         var clickListener = new DefaultClickListener(v =>
         {
@@ -61,13 +67,16 @@ public class AuthSignInFragment : BaseFragment
             .To(vm => vm.SignIn.Password)
             .OneWayToSource();
 
-        // set.Bind(signIn.SignInButton)
-        //     .For(x => x.BindClick())
-        //     .To(vm => vm.SignInCommand);
-        //
-        // set.Bind(signIn.SignInViaVk)
-        //     .For(x => x.BindClick())
-        //     .To(vm => vm.SignInViaVkCommand);
+        // TODO For some reason can't change visibility of this ui elements. Even through straight invokes and UI thread
+        set.Bind(signIn.SignInLoader)
+            .For(x => x.BindVisible())
+            .To(vm => vm.State)
+            .WithConversion(new AnyExpressionConverter<AuthViewState, bool>(x => x.HasFlag(AuthViewState.SigningIn)));
+
+        set.Bind(signIn.SignInViaVkLoader)
+            .For(x => x.BindVisible())
+            .To(vm => vm.State)
+            .WithConversion(new AnyExpressionConverter<AuthViewState, bool>(x => x.HasFlag(AuthViewState.SigningInViaVk)));
 
         set.Apply();
 
@@ -82,12 +91,16 @@ public class AuthSignInFragment : BaseFragment
             TextInputEditText emailValue,
             TextInputEditText passwordValue,
             MaterialButton signInButton,
-            MaterialButton signInViaVk)
+            CircularProgressIndicator signInLoader,
+            MaterialButton signInViaVk,
+            CircularProgressIndicator signInViaVkLoader)
         {
             EmailValue = emailValue;
             PasswordValue = passwordValue;
             SignInButton = signInButton;
+            SignInLoader = signInLoader;
             SignInViaVk = signInViaVk;
+            SignInViaVkLoader = signInViaVkLoader;
         }
 
         public TextInputEditText EmailValue { get; }
@@ -96,6 +109,10 @@ public class AuthSignInFragment : BaseFragment
 
         public MaterialButton SignInButton { get; }
 
+        public CircularProgressIndicator SignInLoader { get; }
+
         public MaterialButton SignInViaVk { get; }
+
+        public CircularProgressIndicator SignInViaVkLoader { get; }
     }
 }
