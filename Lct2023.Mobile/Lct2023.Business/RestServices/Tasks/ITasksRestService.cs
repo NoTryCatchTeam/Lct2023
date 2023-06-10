56c206881e317cde46f996a207b1dc1a278aceb1
Dmitry Ordenov <dordenov@artec3d.com>
2023-06-10 11:16:19 +0200

    added task of the day

--- Lct2023.Mobile/Lct2023.Business/RestServices/Tasks/ITasksRestService.cs
+++ Lct2023.Mobile/Lct2023.Business/RestServices/Tasks/ITasksRestService.cs
@@ -9,4 +9,6 @@ namespace Lct2023.Business.RestServices.Tasks;
 public interface ITasksRestService
 {
     Task<IEnumerable<CmsItemResponse<TaskItemResponse>>> GetTasksAsync(CancellationToken token);
+
+    Task<IEnumerable<CmsItemResponse<TaskOfTheDayItemResponse>>> GetTaskOfTheDayAsync(CancellationToken token);
 }
